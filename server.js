010647c04a0fdb2326ea2a34311e15d836429594
Leslie Book <lesliek.book@gmail.com>
2021-02-16 18:02:59 -0500

    Commit with resolved pathing issues and missing css path

--- server.js
+++ server.js
@@ -1,21 +1,79 @@
-// Require Dependencies
 const express = require("express");
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
+const database = require("./db/db")
 
-// Initialize express app
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Parsing
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname));
 
-//Routes
-require('./routes/routes')(app);
+app.get("/", function(req, res) {
 
-// Port listener
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+})
+
+app.route("/api/notes")
+    .get(function(req, res) {
+        res.json(database);
+    })
+
+.post(function(req, res) {
+    let jsonFilePath = path.join(__dirname, "/db/db.json");
+    let newNote = req.body;
+
+    let highestId = 99;
+
+    for (let i = 0; i < database.length; i++) {
+        let individualNote = database[i];
+
+        if (individualNote.id > highestId) {
+
+            highestId = individualNote.id;
+        }
+    }
+
+    newNote.id = highestId + 1;
+    database.push(newNote)
+
+    fs.writeFile(jsonFilePath, JSON.stringify(database), function(err) {
+
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Note added");
+    });
+
+    res.json(newNote);
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+    let jsonFilePath = path.join(__dirname, "/db/db.json");
+    for (let i = 0; i < database.length; i++) {
+
+        if (database[i].id == req.params.id) {
+            database.splice(i, 1);
+            break;
+        }
+    }
+
+    fs.writeFileSync(jsonFilePath, JSON.stringify(database), function(err) {
+
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log("Note deleted");
+        }
+    });
+    res.json(database);
+});
+
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+    console.log("App listening on PORT " + PORT);
 });
